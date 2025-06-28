7ba3255822136d6abac8570811bd473b5de2e977
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-06-28 17:17:38 +0300

    Remove Assertify library and related test files

--- test/test_assert_error_class.cpp
+++
@@ -1,7 +0,0 @@
-#include "assertify.hpp"
-
-int main(int argc, char *argv[])
-{
-    int x = 0;
-    ASSERTIFY_ASSERT_EXCEPTION(x > 0, "x must be positive");
-}
--- test/test_long_jump_style_assert.cpp
+++
@@ -1,8 +0,0 @@
-#define ASSERTIFY_LONG_JMP_ENDABLED
-#include "assertify.hpp"
-
-int main(int argc, char *argv[])
-{
-    int x = 0;
-    ASSERTIFY_ASSERT_EXCEPTION(x > 0, "x must be positive");
-}
