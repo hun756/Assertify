67849a49449206cb095f44d8250d017200784be5
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-07-08 15:08:05 +0300

    test: Add StringLikeFormatterTest for various string formatting scenarios

--- test/formatter_test.cpp
+++ test/formatter_test.cpp
@@ -71,3 +71,58 @@ struct ThrowingStreamableType
 } // namespace testing_utils
 
 using namespace testing_utils;
+
+class StringLikeFormatterTest : public ValueFormatterTest
+{
+};
+
+TEST_F(StringLikeFormatterTest, StdStringFormatting)
+{
+    std::string test_str = "Hello, World!";
+    auto result = value_formatter<std::string>::format(test_str);
+
+    EXPECT_EQ(result, "\"Hello, World!\"");
+    EXPECT_TRUE(uses_thread_local_allocator(result));
+}
+
+TEST_F(StringLikeFormatterTest, EmptyStringFormatting)
+{
+    std::string empty_str = "";
+    auto result = value_formatter<std::string>::format(empty_str);
+
+    EXPECT_EQ(result, "\"\"");
+}
+
+TEST_F(StringLikeFormatterTest, StringViewFormatting)
+{
+    std::string_view test_view = "string_view test";
+    auto result = value_formatter<std::string_view>::format(test_view);
+
+    EXPECT_EQ(result, "\"string_view test\"");
+}
+
+TEST_F(StringLikeFormatterTest, CStringFormatting)
+{
+    const char* test_cstr = "C-style string";
+    auto result = value_formatter<const char*>::format(test_cstr);
+
+    EXPECT_EQ(result, "\"C-style string\"");
+}
+
+TEST_F(StringLikeFormatterTest, StringWithSpecialCharacters)
+{
+    std::string special = "Line1\nLine2\tTabbed\"Quoted\"";
+    auto result = value_formatter<std::string>::format(special);
+
+    EXPECT_EQ(result, "\"Line1\nLine2\tTabbed\"Quoted\"\"");
+}
+
+TEST_F(StringLikeFormatterTest, FastStringFormatting)
+{
+    fast_string<char> fast_str("fast string test",
+                               tl_pool.get_allocator<char>());
+    auto result = value_formatter<fast_string<char>>::format(fast_str);
+
+    EXPECT_EQ(result, "\"fast string test\"");
+    EXPECT_TRUE(uses_thread_local_allocator(result));
+}
